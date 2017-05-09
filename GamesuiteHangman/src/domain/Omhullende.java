8b6090dfcbd0d4d44c190444387ae946e6de45bd
Shiva Janssens <shivajanssens@MacBook-Pro-van-Shiva.local>
2017-05-09 14:23:28 +0200

    begin Omhullende

---
+++ GamesuiteHangman/src/domain/Omhullende.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Omhullende {
+	private Punt positieLinksBoven;
+	private int breedte, hoogte;
+	
+	public Omhullende(Punt positieLinksBoven, int breedte, int hoogte) {
+		
+	}
+	
+	
+}
