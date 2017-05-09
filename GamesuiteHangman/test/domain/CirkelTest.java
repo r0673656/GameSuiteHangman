7b74133d256c4c8ef102a36e7158e5e3d6717055
FelixRules <felix.roels@studen.ucll.be>
2017-05-09 11:23:19 +0200

    Testnamen geschreven

--- GamesuiteHangman/test/domain/CirkelTest.java
+++ GamesuiteHangman/test/domain/CirkelTest.java
@@ -1,5 +1,40 @@
 package domain;
 
-public class CirkelTest {
+import org.junit.Test;
 
+public class CirkelTest {
+	
+	
+	@Test
+	public void Cirkel_aanmaken_met_geldig_middelpunt_en_geldige_straal(){
+		
+	}
+	@Test
+	public void Exception_wanneer_cirkel_aanmaken_met_middelpunt_null(){
+		
+	}
+	@Test 
+	public void Exception_cirkel_aanmaken_kleiner_nul(){
+		
+	}
+	@Test 
+	public void Exception_cirkel_aanmaken_straal_is_nul(){
+		
+	}
+	@Test
+	public void Cirkels_gelijk_zelfde_middelpunt_zelfde_straal(){
+		
+	}
+	@Test
+	public void Cirkels_anders_wanneer_tweede_null(){
+		
+	}
+	@Test
+	public void Cirkels_anders_wanneer_middelpunt_verschillend(){
+		
+	}
+	@Test
+	public void Cirkels_verschillend_wanneer_straal_verschillend(){
+		
+	}
 }
