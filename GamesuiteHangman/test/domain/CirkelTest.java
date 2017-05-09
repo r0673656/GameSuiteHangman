d0425c081d5257bfd403662d66b84bf5997571d7
FelixRules <felix.roels@studen.ucll.be>
2017-05-09 11:46:06 +0200

    aanpassingen test 1

--- GamesuiteHangman/test/domain/CirkelTest.java
+++ GamesuiteHangman/test/domain/CirkelTest.java
@@ -1,25 +1,35 @@
 package domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CirkelTest {
 	
+	private Punt middelpunt;
+	private int straal;
+	private Cirkel cirkel;
+	
+	@Before
+	public void setUp() throws Exception{
+		cirkel = new Cirkel(middelpunt, straal);
+	}
 	
 	@Test
 	public void Cirkel_aanmaken_met_geldig_middelpunt_en_geldige_straal(){
+		Cirkel cirkel = new Cirkel();
 		
 	}
-	@Test
+	@Test (expected = IllegalArgumentException.class)
 	public void Exception_wanneer_cirkel_aanmaken_met_middelpunt_null(){
-		
+		cirkel = new Cirkel(null, straal);
 	}
-	@Test 
+	@Test (expected = IllegalArgumentException.class)
 	public void Exception_cirkel_aanmaken_kleiner_nul(){
-		
+		cirkel = new Cirkel(middelpunt, -1);
 	}
-	@Test 
+	@Test (expected = IllegalArgumentException.class)
 	public void Exception_cirkel_aanmaken_straal_is_nul(){
-		
+		cirkel = new Cirkel(middelpunt,0);
 	}
 	@Test
 	public void Cirkels_gelijk_zelfde_middelpunt_zelfde_straal(){
