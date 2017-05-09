422b202b5b586e4f51eddd061c24af8167a32ea0
FelixRules <felix.roels@studen.ucll.be>
2017-05-09 13:38:43 +0200

    aanmaking Driehoek

---
+++ GamesuiteHangman/src/domain/Driehoek.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Driehoek {
+	private Punt hoekPunt1;
+	private Punt hoekPunt2;
+	private Punt hoekPunt3;
+	
+	public Driehoek(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3){
+		
+	}
+		
+}
+
