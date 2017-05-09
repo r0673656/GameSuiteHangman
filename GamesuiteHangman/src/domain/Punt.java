744200645f226eb6761430cf2a823660cb5946ff
Shiva Janssens <shivajanssens@MacBook-Pro-van-Shiva.local>
2017-05-09 11:22:15 +0200

    Punt classe

--- GamesuiteHangman/src/domain/Punt.java
+++ GamesuiteHangman/src/domain/Punt.java
@@ -1,5 +1,43 @@
 package domain;
 
 public class Punt {
+	private int x, y;
+	
+	public Punt(int x, int y) {
+		setX(x);
+		setY(y);
+	}
 
+	//---------------- GETTERS / SETTERS ----------------//
+	
+	public int getX() {
+		return x;
+	}
+
+	private void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	private void setY(int y) {
+		this.y = y;
+	}
+	
+	//-------------------- METHODES --------------------//
+	
+	public boolean equals(Punt punt) {
+		if(punt.getX() == this.x && punt.getY() == this.y) { 
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return "(" + this.x + "," + this.y + ")";
+	}
+	
 }
