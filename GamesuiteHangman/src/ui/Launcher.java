ca8a4da917df4e31a88d88a0ca334649cb446bbf
FelixRules <felix.roels@studen.ucll.be>
2017-05-09 15:42:46 +0200

    Lancher + rechthoek, nog niet helemaal in orde

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 
 import domain.Cirkel;
 import domain.Punt;
+import domain.Rechthoek;
 import domain.Speler;
 
 public class Launcher {
@@ -16,6 +17,20 @@ public class Launcher {
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 		JOptionPane.showMessageDialog(null, "... heeft als score: " + speler.getScore());
 		
+		//aanpassingen van Rechthoek, vraagt aan speler of die rechthoek of cirkel wil aanmaken
+		Object [] shapes = {"Cirkel", "Rechthoek"};
+				Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen?", "input"
+						, JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
+		//maakt linkerbovenhoek en rechthoek aan
+		int xco = Integer.parseInt(JOptionPane.showInputDialog("x-coördinaat van de linkerbovenhoek:"));
+		int yco = Integer.parseInt(JOptionPane.showInputDialog("y-coördinaat van de linkerbovenhoek:"));
+		Punt linkerbovenhoek = new Punt(xco,yco);		
+		int breedte = Integer.parseInt(JOptionPane.showInputDialog("Breedte van de rechthoek: "));
+		int hoogte = Integer.parseInt(JOptionPane.showInputDialog("Hoogte van de rechthoek: "));
+		Rechthoek rechthoek = new Rechthoek(linkerbovenhoek, breedte, hoogte);
+		JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt: " + rechthoek);
+		
+		//aanpassingen punt en cirkel
 		int x = Integer.parseInt(JOptionPane.showInputDialog("x-coördinaat van het punt:"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("y-coördinaat van het punt:"));
 		Punt punt = new Punt(x,y);
@@ -24,6 +39,8 @@ public class Launcher {
 		int straal = Integer.parseInt(JOptionPane.showInputDialog("Radius van de cirkel: "));
 		Cirkel cirkel = new Cirkel(punt, straal);
 		JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: "+ cirkel);
+		
+		System.out.println(keuze);
 	}
 
 }
