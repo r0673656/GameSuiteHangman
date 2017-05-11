f725aba398ae4091c5c1767718d285c50f7fd60a
DESKTOP-L54FU5O\Rune <rune.hanssens@hotmail.be>
2017-05-11 14:21:09 +0200

    story 12 klaar

--- GamesuiteHangman/src/domain/HintLetter.java
+++ GamesuiteHangman/src/domain/HintLetter.java
@@ -2,7 +2,8 @@ package domain;
 
 public class HintLetter {
 	private char letter;
-
+	private boolean isGeraden;
+	
 	public HintLetter(char letter) {
 		setLetter(letter);
 	}
@@ -15,5 +16,30 @@ public class HintLetter {
 		this.letter = letter;
 	}
 	
+	protected void setIsGeraden(boolean isGeraden){
+		this.isGeraden = isGeraden;
+	}
 	
+	public boolean isGeraden(){
+		return this.isGeraden;
+	}
+	
+	public boolean raad(char letter){
+		if((Character.toUpperCase(this.letter) == letter)||(Character.toLowerCase(this.letter) == letter)){
+			if(isGeraden) return false;
+			this.isGeraden = true;
+			return true;
+		}
+		return false;
+	}
+	
+	public char toChar(){
+		String string = "_";
+		char ch = string.charAt(0);
+		if(isGeraden){
+			return this.letter;
+		}else{
+			return ch;
+		}
+	}
 }
--- GamesuiteHangman/src/domain/HintWoord.java
+++ GamesuiteHangman/src/domain/HintWoord.java
@@ -1,5 +1,62 @@
 package domain;
 
-public class HintWoord {
+import java.util.ArrayList;
 
+public class HintWoord {
+	private String woord;
+	private ArrayList<HintLetter> hint = new ArrayList<HintLetter>();
+	public HintWoord(String woord) {
+		setWoord(woord);
+		setHint(woord);
+	}
+
+	public String getWoord() {
+		return woord;
+	}
+
+	private void setWoord(String woord) {
+		if(woord == null || woord.trim().isEmpty()){
+			throw new DomainException("woord is null!");
+		}
+		this.woord = woord;
+	}
+	
+	private void setHint(String woord){
+		for (int i = 0; i < woord.length(); i ++){
+			hint.add(new HintLetter(woord.charAt(i)));
+		}
+	}
+	
+	public String toString(){
+		String result ="";
+		for(int i = 0; i < woord.length(); i++){
+			result += this.hint.get(i).toChar();
+			if(i < woord.length()-1){
+				result += " ";
+			}
+		}
+		return result;
+	}
+	
+	public boolean isGeraden(){
+		boolean result = true;
+		for(int i = 0; i < woord.length(); i++){
+			if(this.hint.get(i).isGeraden() == false){
+				result = false;
+			}
+		}
+		return result;
+	}
+	
+	public boolean raad(char letter){
+		boolean result = false;
+		for(int i = 0; i < woord.length(); i ++){
+			if(Character.toLowerCase(this.hint.get(i).getLetter()) == letter || Character.toUpperCase(this.hint.get(i).getLetter()) == letter){
+				if(this.hint.get(i).isGeraden()) return false;
+				result = true;
+				this.hint.get(i).setIsGeraden(true);
+			}
+		}
+		return result;
+	}
 }
