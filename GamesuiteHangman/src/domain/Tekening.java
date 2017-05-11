792f80c98bde868300e6257fa877669723cabe3a
Keanu_Tastenhoye <Keanu.Tastenhoye@student.ucll.be>
2017-05-11 13:21:26 +0200

    goeie versie van tekening

--- GamesuiteHangman/src/domain/Tekening.java
+++ GamesuiteHangman/src/domain/Tekening.java
@@ -13,13 +13,10 @@ public class Tekening{
 	public ArrayList<Vorm> vormen = new ArrayList<>();
 	
 	public Tekening(String naam){
-		
-		
 		if(naam == null || naam.trim().isEmpty()){
 			throw new IllegalArgumentException("");
 		}
 		this.naam = naam;
-		
 	}
 	public String getNaam() {
 		return naam;
@@ -28,23 +25,16 @@ public class Tekening{
 		vormen.add(vorm);
 	}
 	public Vorm getVorm(int index){
-		
 		return vormen.get(index);
-		
-		
 	}
 	public int getAantalVormen(){
-		
 		return vormen.size();
-		
 	}
 	public ArrayList<Vorm> getAlleVormen(){
 		return vormen;
 	}
 	public void verwijder(Vorm vorm){
-		
 		vormen.remove(vorm);
-		
 	}
 	public boolean bevat(Vorm vorm){
 		return vormen.contains(vorm);
@@ -53,19 +43,24 @@ public class Tekening{
 		return null;
 	}
 	@Override
-	public boolean equals(Object object){
-		boolean result = false;
-		if(object instanceof Tekening){
-			Tekening t = (Tekening) object;
-			
-			if(this.getAantalVormen() == (t.getAantalVormen()) && (this.getNaam().equals(t.getNaam()))){
-				result = true;
+	public boolean equals(Object object) {
+		if(object instanceof Tekening) {
+			Tekening tekening = (Tekening) object;
+			if(tekening.getAantalVormen() == this.getAantalVormen()) {
+				int count = 0;
+				for(int i = 0; i < vormen.size(); i++) {
+					for(int j = 0; j < vormen.size(); j++) {
+						if(this.getVorm(i).getClass().equals(tekening.getVorm(j).getClass()) && this.getVorm(i).equals(tekening.getVorm(j))) {
+							count++;
+						}
+					}
+				}
+				if(count != this.getAantalVormen()) return false;
+				return true;
 			}
-			
 		}
-		
-	return result;	
-		
+		return false;
 	}
+
 	
 }
