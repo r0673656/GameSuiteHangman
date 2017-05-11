4cc3b24fd6762d58dc7bcf1059b40b47025b09f1
FelixRules <felix.roels@studen.ucll.be>
2017-05-11 14:02:09 +0200

    story 11 gegeven

--- GamesuiteHangman/src/ui/GameMainWindow.java
+++ GamesuiteHangman/src/ui/GameMainWindow.java
@@ -1,30 +1,48 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import domain.Tekening;
 
 public class GameMainWindow extends JFrame{
 	
-	public GameMainWindow(){
-			this.setTitle("Hangman Suite - Projectweek 2017");
-			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		    this.setSize(dim.width, dim.height - 40); // minus task bar
-		    this.setResizable(false);  
-		    
-		    Tekening tekening = new Tekening("Tekening");
-		    this.setLayout(new BorderLayout());
-		    
-		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    this.setVisible(true);
-		   	     
-		    
-		    
-		    
+private static final long serialVersionUID = 1L;
+	
+	private TekenVenster tekenvenster;
+
+	public GameHoofdScherm(String titel, Tekening tekening){
+		super(titel);
+		TekenVenster tekenvenster = new TekenVenster(tekening);
+		setTekenvenster(tekenvenster);
+
+		JPanel tekeningPaneel = new JPanel();
+		tekeningPaneel.setBackground(Color.white);
+		tekeningPaneel.setBorder(BorderFactory.createMatteBorder( 3, 3, 3, 3, Color.darkGray));
+		tekeningPaneel.add(tekenvenster);
+
+		this.setLayout(new BorderLayout());
+		this.add(tekeningPaneel, BorderLayout.CENTER);
+		this.pack();
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void teken(){
+		getTekenvenster().teken();
 	}
 
+	protected TekenVenster getTekenvenster() {
+		return tekenvenster;
+	}
+
+	private void setTekenvenster(TekenVenster tekenvenster) {
+		this.tekenvenster = tekenvenster;
+	}
 }
